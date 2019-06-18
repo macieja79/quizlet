a45a25455c8b7a3c1e5d1635b8c1bd8c96a08f96
Maciej Szczudlo <maciej.szczudlo@ercisson.com>
2019-06-18 10:25:31 +0200

    Add integr tests

--- Metaproject.Quiz.Test.IntegrationTests/WordTests.cs
+++ Metaproject.Quiz.Test.IntegrationTests/WordTests.cs
@@ -1,7 +1,7 @@
+using FluentAssertions;
 using Metaproject.Quiz.Inf.WordDocsRepository;
 using Metaproject.Quiz.Test.IntegrationTests.Helpers;
 using NUnit.Framework;
-
 
 namespace Tests
 {
@@ -13,35 +13,40 @@ namespace Tests
         }
 
         [Test]
-        public void CheatsheetTable()
+        public void WordFilesRepository_Should_Extract_Cheatsheet_Table_From_Docx()
         {
             var wordDocRepo = new WordFilesRepository();
 
             var rootPath = IntegrationTestsHelper.GetFullResourcePath("integration-test-cheatsheet-table.docx");
 
-            var docs = wordDocRepo.GetDocument(rootPath);
+            var doc = wordDocRepo.GetDocument(rootPath);
 
-            
-
+            doc.Should().NotBeNull();
+            doc.Tables.Should().HaveCount(3);
+            doc.Tables[0].Question.Should().Be(@"Test question 1/3");
+            doc.Tables[0].Answer.Should().Be(@"cmd answer –demo1");
+            doc.Tables[1].Question.Should().Be(@"Test question 2/3");
+            doc.Tables[1].Answer.Should().Be(@"cmd answer –demo2");
+            doc.Tables[2].Question.Should().Be(@"Test question 3/3");
+            doc.Tables[2].Answer.Should().Be(@"cmd answer –demo3");
         }
 
         [Test]
-        public void QuestionTable()
+        public void WordFilesRepository_Should_Extract_Question_Table_From_Docx()
         {
             var wordDocRepo = new WordFilesRepository();
 
             var rootPath = IntegrationTestsHelper.GetFullResourcePath("integration-test-question-table.docx");
 
-            var docs = wordDocRepo.GetDocument(rootPath);
+            var doc = wordDocRepo.GetDocument(rootPath);
 
-            //var tables = docs.SelectMany(d => d.Tables).ToList();
+            doc.Should().NotBeNull();
+            doc.Tables.Should().HaveCount(1);
+            doc.Tables[0].Question.Should().Be("Test question");
+            doc.Tables[0].Answers[0].Should().Be("Test answer 1");
+            doc.Tables[0].Answers[1].Should().Be("Test answer 2");
+            doc.Tables[0].Answers[2].Should().Be("Test answer 3");
         }
-
-
-
-
-
        
     }
-
 }
